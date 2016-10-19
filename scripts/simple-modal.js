9778cb2691ff00a43c4cd047dd92c2ce3d27c306
Saman Tehrani <samanrtehrani@gmail.com>
2016-10-18 23:12:06 -0400

    what if we had one modal and content could change

--- scripts/simple-modal.js
+++ scripts/simple-modal.js
@@ -1,17 +1,19 @@
 let modals = document.querySelectorAll('.modal');
-let modalPanels = document.querySelectorAll('.modal-panel');
-for (modalPanel of modalPanels) {
-    modalPanel.addEventListener('click', function(e) {
-        if (e.target === this)
-            this.style.display = "none";
-    }, false);
-}
+let modalPanel = document.querySelector('#modal-panel');
+modalPanel.addEventListener('click', function(e) {
+    if (e.target === this)
+        this.style.display = "none";
+}, false);
 
 for (let modal of modals) {
     modal.addEventListener('click', function() {
-        let contentId = this.id;
+        let contentId = this.dataset.modalContent;
         console.log(contentId);
-        let modalPanel = document.querySelector(`#${contentId}.modal-panel`);
+        let modalContent = document.querySelector(`#${contentId}`).cloneNode();
+        modalContent.classList.add('open');
+        modalPanel.innerHTML = '';
+
+        modalPanel.appendChild(modalContent);
         modalPanel.style.display = "flex";
     }, false);
 }
