0a2e6cda62145a4f8819929f66b27cff0304e091
Saman Tehrani <samanrtehrani@gmail.com>
2016-10-18 23:15:38 -0400

    whatifs ???

--- scripts/simple-modal.js
+++ scripts/simple-modal.js
@@ -1,3 +1,9 @@
+//what if we wanted to have limitless modals?
+//what if we wanted to have chained modals?
+//what if we wanted different sizes?
+//what if the content was from another file?
+//what if we wanted other methods for closing/opening the modal?
+//what if we wanted the user to be able to use arbitrary selectors names?
 let modals = document.querySelectorAll('.modal');
 let modalPanel = document.querySelector('#modal-panel');
 modalPanel.addEventListener('click', function(e) {
