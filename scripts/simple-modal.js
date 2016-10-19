89cfbf78a40e46844295bacc7fa3955faba99916
Saman Tehrani <samanrtehrani@gmail.com>
2016-10-18 21:33:45 -0400

    what if I had multiple buttons that trigger modal

--- scripts/simple-modal.js
+++ scripts/simple-modal.js
@@ -1,10 +1,12 @@
-let modal = document.querySelector('.modal');
+let modals = document.querySelectorAll('.modal');
 let modalPanel = document.querySelector('#modal-panel');
 modalPanel.addEventListener('click', function(e) {
     if (e.target === this)
         modalPanel.style.display = "none";
 
 }, false);
-modal.addEventListener('click', function() {
-    modalPanel.style.display = "flex";
-}, false);
+for (let modal of modals) {
+    modal.addEventListener('click', function() {
+        modalPanel.style.display = "flex";
+    }, false);
+}
